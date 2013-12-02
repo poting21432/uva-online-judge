447623f7d97d893bd2572a14365195b9c1052309
Jiahe Liao <stqhmf@gmail.com>
2013-12-02 22:30:23 +0800

    problem list

--- misc/uva-link.cpp
+++ misc/uva-link.cpp
@@ -7,12 +7,13 @@ char buf[1024];
 int main()
 {
   while ( gets(buf) ) {
-    if ( strlen(buf) > 10 && isdigit(buf[0]) && isdigit(buf[1]) ) {
+    if ( strlen(buf)>3 && isdigit(buf[0]) && isdigit(buf[1]) ) {
       char problem_id[16];
-      sscanf(buf, "* %s", problem_id);
+      sscanf(buf, "%s", problem_id);
       char problem_dir[16];
       strcpy(problem_dir, problem_id);
       problem_dir[strlen(problem_id)-2] = '\0';
+      //printf("%s / %s\n", problem_dir, problem_id);
       printf("* [%s](http://uva.onlinejudge.org/external/%s/%s.html)\n", buf, problem_dir, problem_id);
     }
     else {
